6b0b46ec0948befe811ad09159f6441903cee9e5
Anand Tripathi <atripath@cs.utah.edu>
2017-12-01 12:58:18 -0700

    Fixes to last commit

--- MLApp/google_result_crawler.py
+++ MLApp/google_result_crawler.py
@@ -9,14 +9,14 @@ def get_google_results(searchTerm, pages):
     driver.get("http://www.google.com")
 
     # Execute the search
-    page_number = 2
+    page_number = 1
     searchField = driver.find_element_by_name("q")
     searchField.send_keys(searchTerm)
     searchButton = driver.find_element_by_name("btnK")
     searchButton.click()
 
     # Iterate through pages
-    while page_number <= pages:
+    while True:
         try:
             # Gather search results
             results = driver.find_elements_by_css_selector(".r")
@@ -25,10 +25,15 @@ def get_google_results(searchTerm, pages):
                     print each.find_element_by_css_selector("a").get_attribute("href")
                 except:
                     pass
-            driver.find_element_by_link_text(str(page_number)).click()
+            if page_number < pages:
+                driver.find_element_by_link_text(str(page_number+1)).click()
+                page_number += 1
+            else:
+                break
         except Exception as E:
             raise E
-        page_number += 1
+    # Close Browser
+    driver.close()
 
 
 def main():
